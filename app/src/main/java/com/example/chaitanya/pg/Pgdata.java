a768080cc556db79ffb49b0b644f4b5b0ea96dea
chaitanyaparashar <cparashar007@gmail.com>
2017-10-16 19:01:17 +0530

    next and previous buttons
    imageslider updated

--- app/src/main/java/com/example/chaitanya/pg/Pgdata.java
+++ app/src/main/java/com/example/chaitanya/pg/Pgdata.java
@@ -6,15 +6,18 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by chaitanya on 14-Oct-17.
  */
 
-public class Pgdata extends AppCompatActivity{
+public class Pgdata extends AppCompatActivity implements View.OnClickListener {
     ViewPager viewPager;
     Bitmap bmp;
     MyCustomAdapter myCustomPagerAdapter;
+    Button b1, b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +25,10 @@ public class Pgdata extends AppCompatActivity{
         setContentView(R.layout.pgdata);
         byte[] byteArray = getIntent().getByteArrayExtra("id");
         bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
-
+        b1 = (Button) findViewById(R.id.btt1);
+        b2 = (Button) findViewById(R.id.btt2);
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
         viewPager = (ViewPager) findViewById(R.id.viewPager);
 
         myCustomPagerAdapter = new MyCustomAdapter(this, bmp);
@@ -33,6 +39,21 @@ public class Pgdata extends AppCompatActivity{
     public boolean onCreateOptionsMenu(Menu menu) {
 
         return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            //prev button
+            case R.id.btt2:
+                viewPager.setCurrentItem(viewPager.getCurrentItem() - 1, true);
+                break;
+            //nextbutton, 1st argument : pageindex, 2nd argument : Enable smooth
+            case R.id.btt1:
+                viewPager.setCurrentItem(viewPager.getCurrentItem() + 1, true);
+                break;
+        }
     }
 }
 
